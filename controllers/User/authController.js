ddf1dc98583577bd445332ee0999e3643df8bc9a
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-03-09 00:22:50 +0530

    register user

--- controllers/User/authController.js
+++ controllers/User/authController.js
@@ -1,6 +1,33 @@
+const User = require('../../models/User/User')
+const CustomError = require("../../errors");
+const { StatusCodes } = require("http-status-codes");
 
 const registerUser = async(req, res)=>{
- res.send('register')
+  const {firstname, lastname, email, phone, password} = req.body
+  if(!firstname || !lastname || !email || !phone || !password){
+    throw new CustomError.BadRequestError('Please provide all values!')
+  }
+  const isUserExists = await User.findOne({phone:phone})
+  if(isUserExists){
+    throw new CustomError.BadRequestError('User already exists!')
+  }
+  const user = await User.create({
+    firstname,
+    lastname,
+    email,
+    phone,
+    password
+  })
+  newUser = {
+    firstname:user.firstname,
+    lastname:user.lastname,
+    email:user.email,
+    phone:user.phone,
+    role:user.role
+  }
+  const token = user.createJWT();
+
+ res.status(StatusCodes.CREATED).json({user:newUser, token})
 }
 
 module.exports = {
