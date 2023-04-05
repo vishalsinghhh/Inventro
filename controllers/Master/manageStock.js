7f79c5331daa0c6cd3793f6a207eba5362abd2c0
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-04-06 00:34:50 +0530

    sub category

--- controllers/Master/manageStock.js
+++ controllers/Master/manageStock.js
@@ -1,18 +1,61 @@
-const MasterStock = require('../../models/Master/MasterStock')
+const MasterStock = require("../../models/Master/MasterStock");
+const MasterSubcategory = require("../../models/Master/MasterSubcategory");
 const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../../errors");
 
-const addStockToInventory = async(req, res)=>{
-    
-}
+const addCategory = async (req, res) => {
+  const { category } = req.body;
+  if (!category) {
+    throw new CustomError.BadRequestError("Please provide category!");
+  }
+  const isCategoryExists = await MasterStock.findOne({
+    master: req.user.userId,
+    category: category,
+  });
+  if (isCategoryExists) {
+    throw new CustomError.BadRequestError("This category already exists!");
+  }
+  const newCategory = await MasterStock.create({
+    master: req.user.userId,
+    category,
+  });
 
-const getStock = async(req, res)=>{
-    const {id:product_id} = req.params
-    const item = await MasterStock.findOne({_id:product_id})
-    console.log(item);
-    res.send(item)
-}
+  res.status(StatusCodes.CREATED).json({ newCategory });
+};
 
-module.exports ={
-    addStockToInventory,
-    getStock
-}
+const addSubCategory = async (req, res) => {
+  const { id: categoryId } = req.params;
+  const { subcategory } = req.body;
+  if(!subcategory){
+    throw new CustomError.BadRequestError('Please provide subcategory')
+  }
+  const isSubCategoryExists = await MasterSubcategory.findOne({
+    subcategory: subcategory,
+  })
+  if(isSubCategoryExists){
+    throw new CustomError.BadRequestError("This category already exists")
+  }
+
+  const newCategory = await MasterSubcategory.create({
+    category:categoryId,
+    subcategory
+  });
+
+  res.status(StatusCodes.CREATED).json({newCategory})
+} 
+
+const addStockToInventory = async (req, res) => {};
+
+const getStock = async (req, res) => {
+  const { id: product_id } = req.params;
+  const item = await MasterStock.findOne({ _id: product_id });
+  console.log();
+  res.send(item);
+};
+
+module.exports = {
+  addStockToInventory,
+  getStock,
+  addCategory,
+  addSubCategory,
+};
--- routes/Master/stockRoutes.js
+++ routes/Master/stockRoutes.js
@@ -4,9 +4,11 @@ const {
   authenticateUser,
 } = require("../../middleware/authentication");
 
-const {addStockToInventory, getStock} = require('../../controllers/Master/manageStock')
+const {addStockToInventory, getStock, addCategory, addSubCategory} = require('../../controllers/Master/manageStock')
 
 router.route("/addStock").post(authenticateUser,addStockToInventory )
+router.route("/addCategory").post(authenticateUser,addCategory )
+router.route("/addSubcategory/:id").post(authenticateUser,addSubCategory )
 router.route("/:id").get(authenticateUser,getStock )
 
 module.exports = router;
