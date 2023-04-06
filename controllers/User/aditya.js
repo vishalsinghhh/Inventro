4523a61b09609aa9178898265183b68723cef6f0
vishalsinghhh <vishalsingh0710vs@gmail.com>
2023-04-07 02:42:21 +0530

    new changes

--- controllers/User/aditya.js
+++ controllers/User/aditya.js
@@ -1,26 +1,77 @@
 const { auth, db } = require("../../config");
 const { ref, onValue, set } = require("firebase/database");
-const {signInWithEmailAndPassword} = require("firebase/auth")
+const { signInWithEmailAndPassword } = require("firebase/auth");
 const { StatusCodes } = require("http-status-codes");
 
 const getData = async (req, res) => {
-    const user = await signInWithEmailAndPassword(auth, 'het@gmail.com', 'hetislegend');
-    set(ref(db, "UsersData/HHeFtnU2LTXWVTBsKqgjwHbJ1pz2/Relay"), {
-        value: 1,
-      });
+  const user = await signInWithEmailAndPassword(
+    auth,
+    "het@gmail.com",
+    "hetislegend"
+  );
+  set(ref(db, "UsersData/HHeFtnU2LTXWVTBsKqgjwHbJ1pz2/Relay"), {
+    value: 1,
+  });
 
-  res.status(StatusCodes.OK).json({msg:"data updated"})
+  res.status(StatusCodes.OK).json({ msg: "data updated" });
 };
 const getData1 = async (req, res) => {
-    const user = await signInWithEmailAndPassword(auth, 'het@gmail.com', 'hetislegend');
-    set(ref(db, "UsersData/HHeFtnU2LTXWVTBsKqgjwHbJ1pz2/Relay"), {
-        value: 0,
-      });
+  const user = await signInWithEmailAndPassword(
+    auth,
+    "het@gmail.com",
+    "hetislegend"
+  );
+  set(ref(db, "UsersData/HHeFtnU2LTXWVTBsKqgjwHbJ1pz2/Relay"), {
+    value: 0,
+  });
 
-  res.status(StatusCodes.OK).json({msg:"data updated"})
+  res.status(StatusCodes.OK).json({ msg: "data updated" });
+};
+
+const getData2 = async (req, res) => {
+    const user = await signInWithEmailAndPassword(
+        auth,
+        "het@gmail.com",
+        "hetislegend"
+      );
+  const hello = ref(
+    db,
+    "UsersData/" + "HHeFtnU2LTXWVTBsKqgjwHbJ1pz2/" + "readings/humidity/"
+  );
+  onValue(hello, (snapshot) => {
+    const data = snapshot.val();
+    const newPosts = Object.keys(data).map((key) => ({
+      id: key,
+      ...data[key],
+    }));
+    res.status(StatusCodes.OK).json({data})
+  });
+  
+};
+const getData3 = async (req, res) => {
+    const user = await signInWithEmailAndPassword(
+        auth,
+        "het@gmail.com",
+        "hetislegend"
+      );
+  const hello = ref(
+    db,
+    "UsersData/" + "HHeFtnU2LTXWVTBsKqgjwHbJ1pz2/" + "readings/temperature/"
+  );
+  onValue(hello, (snapshot) => {
+    const data = snapshot.val();
+    const newPosts = Object.keys(data).map((key) => ({
+      id: key,
+      ...data[key],
+    }));
+    res.status(StatusCodes.OK).json({data})
+  });
+  
 };
 
 module.exports = {
   getData,
-  getData1
+  getData1,
+  getData2,
+  getData3
 };
--- routes/User/adityaRoute.js
+++ routes/User/adityaRoute.js
@@ -4,10 +4,14 @@ const router = express.Router()
 
 const {
     getData,
-    getData1
+    getData1,
+    getData2,
+    getData3
 } = require('../../controllers/User/aditya')
 
-router.get('/', getData)
+router.get('/on', getData)
+router.get('/humidity', getData2)
+router.get('/temperature', getData3)
 router.get('/off', getData1)
 
 module.exports = router
